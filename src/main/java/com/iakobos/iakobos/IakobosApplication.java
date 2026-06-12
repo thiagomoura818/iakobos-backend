d2c5a2ef973e0cc3c2777fd2784230ba653173da
Thiago Moura <thiagovjmoura@gmail.com>
2026-06-12 17:24:59 -0300

    first commit

---
+++ src/main/java/com/iakobos/iakobos/IakobosApplication.java
@@ -0,0 +1,13 @@
+package com.iakobos.iakobos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IakobosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IakobosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iakobos/iakobos/IakobosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iakobos.iakobos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IakobosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
